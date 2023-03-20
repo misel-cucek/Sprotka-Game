c08e39d024e93421573b3cb909c208c953f1c13e
AnejBezjak <anej.bezjak@student.um.si>
2023-03-20 08:48:13 +0100

    Added documentation.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public int numCoins = 0; // the number of coins the player has collected
     public bool isGameOver = false; // whether the game is over or not
     public bool isFinished = false; // whether the game has been finished or not
+    public bool isGameCompleted = false; // whether the game has been completed or not
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +20,20 @@ public class GameManager : MonoBehaviour
         numCoins = 0;
         isGameOver = false;
         isFinished = false;
+        isGameCompleted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameOver)
+        {
+            // Restart game if player presses R
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Restart();
+            }
+        }
     }
     public void CoinCollected()
     {
@@ -44,17 +53,26 @@ public class GameManager : MonoBehaviour
     {
         isGameOver = true;
         // add any additional game over logic here, such as showing a game over screen or resetting the game
+        Debug.Log("Game Over! Final Score: " + score);
+        SceneManager.LoadScene("EndScreen");
     }
 
     public void Finish()
     {
         isFinished = true;
         // add any additional finish logic here, such as showing a finish screen or stopping gameplay
+        if (numCoins == 10)
+        {
+            GameCompleted();
+        }
     }
 
     public void GameCompleted()
     {
+        isGameCompleted = true;
         // add any additional game completed logic here, such as showing a game completed screen or transitioning to a new level
+        Debug.Log("Game Finished! Final Score: " + score);
+        SceneManager.LoadScene("EndScreen");
     }
 
     public void Restart()
@@ -65,6 +83,7 @@ public class GameManager : MonoBehaviour
         numCoins = 0;
         isGameOver = false;
         isFinished = false;
+        isGameCompleted = false;
 
         // reload current scene
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
